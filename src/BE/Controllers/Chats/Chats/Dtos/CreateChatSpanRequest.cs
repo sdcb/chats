1513f74be92db1b55805b592ee93e95ec8760e27
sdcb <sdflysha@qq.com>
2025-03-18 19:10:22 +0800

    Include(x => x.ChatConfig) in UpdateChatSpan

--- src/BE/Controllers/Chats/Chats/Dtos/CreateChatSpanRequest.cs
+++ src/BE/Controllers/Chats/Chats/Dtos/CreateChatSpanRequest.cs
@@ -1,6 +1,4 @@
-﻿using Chats.BE.DB;
-using Chats.BE.DB.Enums;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace Chats.BE.Controllers.Chats.Chats.Dtos;
 
@@ -8,41 +6,4 @@ public record CreateChatSpanRequest
 {
     [JsonPropertyName("modelId")]
     public short ModelId { get; init; }
-}
-
-public record UpdateChatSpanRequest
-{
-    [JsonPropertyName("modelId")]
-    public short ModelId { get; init; }
-
-    [JsonPropertyName("enabled")]
-    public bool Enabled { get; init; }
-
-    [JsonPropertyName("systemPrompt")]
-    public string? SystemPrompt { get; init; }
-
-    [JsonPropertyName("temperature")]
-    public float? Temperature { get; init; }
-
-    [JsonPropertyName("webSearchEnabled")]
-    public bool WebSearchEnabled { get; init; }
-
-    [JsonPropertyName("maxOutputTokens")]
-    public int? MaxOutputTokens { get; init; }
-
-    [JsonPropertyName("reasoningEffort")]
-    public DBReasoningEffort? ReasoningEffort { get; init; }
-
-    public void ApplyTo(ChatSpan span)
-    {
-        span.Enabled = span.Enabled;
-
-        ChatConfig config = span.ChatConfig ?? throw new InvalidOperationException("ChatSpan.ChatConfig is null");
-        config.ModelId = ModelId;
-        config.SystemPrompt = string.IsNullOrEmpty(SystemPrompt) ? null : SystemPrompt;
-        config.Temperature = Temperature;
-        config.WebSearchEnabled = WebSearchEnabled;
-        config.MaxOutputTokens = MaxOutputTokens;
-        config.ReasoningEffort = (byte?)ReasoningEffort;
-    }
 }
