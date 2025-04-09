30184b70a88fb2d661a02418f38b247f9bfefc0e
sdcb <sdflysha@qq.com>
2025-04-09 22:17:26 +0800

    doubao temperature now support 0~2

--- src/scripts/db-migration/2025/20250408-image-gen.sql
+++ src/scripts/db-migration/2025/20250408-image-gen.sql
@@ -5,3 +5,6 @@ VALUES
     (1304, 13, 'gemini-2.0-flash-exp',  'gemini', '2025-03-25', 0, 2, 0, 1, 0, 1, 0, 32768, 8000,  NULL, 0,   0, 'USD'),
     (209,  2,  'hunyuan-t1-latest',     NULL,     '2025-04-03', 0, 2, 1, 0, 1, 1, 1, 64000, 64000, NULL, 1,   4, 'RMB'),
     (210,  2,  'hunyuan-turbos-latest', NULL,     '2025-03-13', 0, 2, 1, 0, 1, 1, 0, 32000, 8000, NULL,  0.8, 2, 'RMB');
+
+-- Release Note - 20250317 https://www.volcengine.com/docs/82379/1159177
+update ModelReference set MaxTemperature = 2 where ProviderId = 16;
