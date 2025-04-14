6f50ed82769ddc99e71733e6201f9c7dd2113810
gray <gray.wen@outlook.com>
2025-04-14 16:21:26 +0800

    adjust ui

--- src/FE/pages/settings/_components/tabs/GeneralTab.tsx
+++ src/FE/pages/settings/_components/tabs/GeneralTab.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
+import React from 'react';
 
 import { useTheme } from 'next-themes';
-import { useRouter } from 'next/router';
 
 import useTranslation from '@/hooks/useTranslation';
 
-import { clearUserInfo, clearUserSession, getLoginUrl } from '@/utils/user';
-
+import { IconDesktop, IconMoon, IconSun } from '@/components/Icons';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Form, FormField } from '@/components/ui/form';
-import FormInput from '@/components/ui/form/input';
-import { FormFieldType, IFormFieldOption } from '@/components/ui/form/type';
-import { Label } from '@/components/ui/label';
-import { IconMoon, IconSun } from '@/components/Icons';
-
-import { changeUserPassword } from '@/apis/clientApis';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
+import { Card, CardContent } from '@/components/ui/card';
 
 const GenerateTab = () => {
   const { t } = useTranslation();
@@ -38,11 +25,7 @@ const GenerateTab = () => {
                 onClick={() => setTheme('system')}
                 className="flex items-center shrink-0"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
-                  <line x1="8" y1="21" x2="16" y2="21"></line>
-                  <line x1="12" y1="17" x2="12" y2="21"></line>
-                </svg>
+                <IconDesktop size={16} className="mr-1" />
                 {t('System')}
               </Button>
               <Button
