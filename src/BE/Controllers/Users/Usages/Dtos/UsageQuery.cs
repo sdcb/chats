1c4f1e0efcaff2d3509fc7dcb8621efc36d8ab4d
sdcb <sdflysha@qq.com>
2025-04-15 12:39:42 +0800

    specify query shortname

--- src/BE/Controllers/Users/Usages/Dtos/UsageQuery.cs
+++ src/BE/Controllers/Users/Usages/Dtos/UsageQuery.cs
@@ -1,18 +1,25 @@
 ﻿using Chats.BE.Controllers.Common.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Chats.BE.Controllers.Users.Usages.Dtos;
 
 public record UsageQuery : PagingRequest
 {
+    [FromQuery(Name = "user")]
     public string? User { get; init; }
 
+    [FromQuery(Name = "kid")]
     public string? ApiKeyId { get; init; }
 
+    [FromQuery(Name = "provider")]
     public string? Provider { get; init; }
 
+    [FromQuery(Name = "start")]
     public DateTime? Start { get; init; }
 
+    [FromQuery(Name = "end")]
     public DateTime? End { get; init; }
 
+    [FromQuery(Name = "tz")]
     public required short TimezoneOffset { get; init; }
 }
