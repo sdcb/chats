7159c66fe6df9c47f5a3ea9a8d9ba3265b6402a2
sdcb <sdflysha@qq.com>
2025-06-26 12:45:46 +0800

    selecting selected message won't set leaf id

--- src/FE/pages/home/_actions/message.actions.ts
+++ src/FE/pages/home/_actions/message.actions.ts
@@ -1,17 +1,16 @@
+import { IChatMessage } from '@/types/chatMessage';
 import {
   MessageAction,
   MessageActionTypes,
-  SetMessagesType,
-  SetSelectedMessagesType,
 } from '../_reducers/message.reducer';
 
-export const setMessages = (messages: SetMessagesType): MessageAction => ({
+export const setMessages = (messages: IChatMessage[]): MessageAction => ({
   type: MessageActionTypes.SET_MESSAGES,
   payload: messages,
 });
 
 export const setSelectedMessages = (
-  selectedMessages: SetSelectedMessagesType,
+  selectedMessages: IChatMessage[][],
 ): MessageAction => ({
   type: MessageActionTypes.SET_SELECTED_MESSAGES,
   payload: selectedMessages,
