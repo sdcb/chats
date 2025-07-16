934cb5963397cf8996ff589f53a53442358b07e5
sdcb <sdflysha@qq.com>
2025-07-16 18:37:12 +0800

    add kimi new models

--- src/scripts/db-migration/1.x/20250716-new-models.sql
+++ src/scripts/db-migration/1.x/20250716-new-models.sql
@@ -1,7 +1,14 @@
 MERGE INTO [ModelReference] AS target
 USING (
     VALUES 
-    (1108, 11, N'grok-4', NULL, '2025-07-09', 0, 2, 1, 1, 1, 1, 1, 262144, 16384, NULL, 3, 15, 'USD')
+    (1108, 11, 'grok-4',                          NULL,   '2025-07-09', 0, 2, 1, 1, 1, 1, 1, 262144, 16384, NULL, 3,  15, 'USD'),
+    (400,  4,  'moonshot-v1-8k',                  'moonshot', null,     0, 1, 0, 0, 1, 1, 0, 8192,   8192,  NULL, 2,  10, 'RMB'),
+    (401,  4,  'moonshot-v1-32k',                 'moonshot', null,     0, 1, 0, 0, 1, 1, 0, 32768,  16384, NULL, 5,  20, 'RMB'),
+    (402,  4,  'moonshot-v1-128k',                'moonshot', null,     0, 1, 0, 0, 1, 1, 0, 131072, 16384, NULL, 10, 30, 'RMB'),
+    (403,  4,  'kimi-latest',                     'kimi',     null,     0, 1, 0, 1, 1, 1, 0, 131072, 16384, NULL, 2,  10, 'RMB'),
+    (404,  4,  'kimi-k2-0711-preview',            'kimi', '2025-07-11', 0, 1, 0, 0, 1, 1, 0, 131072, 16384, NULL, 4,  16, 'RMB'),
+    (1716, 17, 'moonshotai/Kimi-K2-Instruct',     'kimi', '2025-07-11', 0, 1, 0, 0, 1, 1, 0, 131072, 16384, NULL, 4,  16, 'RMB'),
+    (1717, 17, 'Pro/moonshotai/Kimi-K2-Instruct', 'kimi', '2025-07-11', 0, 1, 0, 0, 1, 1, 0, 131072, 16384, NULL, 4,  16, 'RMB')
 ) AS source (
     [Id], [ProviderId], [Name], [DisplayName], [PublishDate], [MinTemperature], [MaxTemperature], 
     [AllowSearch], [AllowVision], [AllowSystemPrompt], [AllowStreaming], [ReasoningResponseKindId], 
