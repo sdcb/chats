ce4f3d90509c3e36a7eea2a4627b2533f3c79a9d
sdcb <sdflysha@qq.com>
2025-09-15 18:49:00 +0800

    improve keycloak compatibility

--- src/BE/Services/Keycloak/SsoTokenDto.cs
+++ src/BE/Services/Keycloak/SsoTokenDto.cs
@@ -7,27 +7,7 @@ public record SsoTokenDto
     [JsonPropertyName("access_token")]
     public required string AccessToken { get; init; }
 
-    [JsonPropertyName("expires_in")]
-    public required int ExpiresIn { get; init; }
-
-    [JsonPropertyName("refresh_expires_in")]
-    public required int RefreshExpiresIn { get; init; }
-
-    [JsonPropertyName("refresh_token")]
-    public required string RefreshToken { get; init; }
-
+    // 可选字段，提高兼容性
     [JsonPropertyName("token_type")]
-    public required string TokenType { get; init; }
-
-    [JsonPropertyName("id_token")]
-    public required string IdToken { get; init; }
-
-    [JsonPropertyName("not-before-policy")]
-    public required int NotBeforePolicy { get; init; }
-
-    [JsonPropertyName("session_state")]
-    public required string SessionState { get; init; }
-
-    [JsonPropertyName("scope")]
-    public required string Scope { get; init; }
+    public string? TokenType { get; init; }
 }
