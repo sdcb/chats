863c948a36f961c8add3f78e27280ba818e98970
sdcb <sdflysha@qq.com>
2025-09-16 12:23:37 +0800

    combine restore script

---
+++ src/scripts/restore/dev.local.sql
@@ -0,0 +1,25 @@
+﻿CREATE CREDENTIAL [s3://io.starworks.cc:88/cv-private]
+WITH
+  IDENTITY = 'S3 Access Key',
+  SECRET = '***:****';
+GO
+
+
+use master
+GO
+ALTER DATABASE [Chats3] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+RESTORE DATABASE [Chats3] 
+FROM URL = N's3://io.starworks.cc:88/cv-private/2025/chats/backup-latest.bak' WITH REPLACE,
+	FILE = 1,
+	MOVE N'Chats3' TO N'D:\mssql-data\Chats3.mdf',
+	MOVE N'Chats3_log' TO N'D:\mssql-data\Chats3.ldf',  
+	--MOVE N'Chats3' TO N'/var/opt/mssql/data/Chats3.mdf',
+	--MOVE N'Chats3_log' TO N'/var/opt/mssql/data/Chats3.ldf',  
+	NOUNLOAD, STATS = 1;
+
+ALTER DATABASE [Chats3] SET MULTI_USER;
+
+use [Chats3];
+GO
+ALTER USER [chats] WITH LOGIN = [chats];
