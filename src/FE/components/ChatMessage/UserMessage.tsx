d6c02220c36e479ce3b0272cf00183ccb77444e3
sdcb <sdflysha@qq.com>
2025-11-26 18:19:42 +0800

    fix strange sub pixel line under UserMessage issue

--- src/FE/components/ChatMessage/UserMessage.tsx
+++ src/FE/components/ChatMessage/UserMessage.tsx
@@ -211,7 +211,7 @@ const UserMessage = (props: Props) => {
             </div>
           </div>
         ) : (
-          <div className="bg-card py-2 px-3 rounded-md overflow-x-scroll">
+          <div className="bg-card py-2 px-3 rounded-md overflow-x-auto">
             <div className="flex flex-wrap justify-end text-right gap-2">
               {content
                 .filter((x) => x.$type === MessageContentType.fileId)
